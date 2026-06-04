85e0f2e37c30a0cc1651b70bc43f014f5bbce8d2
kiarastefan21-tech <kiarastefan21@gmail.com>
2026-06-04 13:04:47 -0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,5 @@
+function startQuiz() {
+    document.getElementById("startPage").style.display = "none";
+    document.getElementById("quizContainer").style.display = "block";
+    document.getElementById("question1").style.display = "block";
+}
